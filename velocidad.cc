33a561e171fe962650f66cb16c11043c6595d918
gbrlrosas <31255645+gbrlrosas@users.noreply.github.com>
2017-11-10 13:47:37 -0600

    Add files via upload

---
+++ velocidad.cc
@@ -0,0 +1,16 @@
+//Rosas Galicia Jose Gabriel
+//introduce una distancia y un tiempo y da una velocidad velocidad=distancia/tiempo
+#include <iostream>
+using namespace std;
+int main(){
+  float v;
+  float d;
+  cout<<"dame una distancia "<<endl;
+  cin>>d;
+  float t;
+  cout<<"dame un tiempo"<<endl;
+  cin>>t;
+  v=d/t;
+  cout<<"velocidad es"<<v<<endl;
+  return 0;
+}
